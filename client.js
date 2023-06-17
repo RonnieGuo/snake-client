93541e57409e68f237aaef96f669f96dd83b9bba
RonnieGuo <guorongfly@gmail.com>
2023-06-17 15:33:56 -0600

    created constants.js to holt IP and PORT

--- client.js
+++ client.js
@@ -1,27 +1,22 @@
-// establishes a connection with the game server
-  // The conn object that Node returned to you represents the connection that you have with the server.
-  // The conn object is full of useful methods and properties that can now be used to interact with the server!
-  // You will use the conn object to handle messages from the server.
-  const net = require("net");
-   const connect = function () {
-    const conn = net.createConnection({
-      host: "localhost",// IP address here,
-      port: 50541,// PORT number here,
-    });
+const net = require("net");
+const { IP, PORT } = require("./constants");
+const connect = function() {
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT
+  });
   
-    //The .on method lets you specify an event name and a function that does something when that event happens.
-    conn.on("connect", () => {
-      // code that does something when the connection is first established
-      console.log('Successfully connected to game server');
-      // Send the name to the server
-      conn.write('Name: ROG');
+//The .on method lets you specify an event name and a function that does something when that event happens.
+  conn.on("connect", () => {
+// code that does something when the connection is first established
+  console.log('Successfully connected to game server');
+// Send the name to the server
+  conn.write('Name: ROG');
       // conn.write('Move: up');
-    });
-  
-    // interpret incoming data as text
-    conn.setEncoding("utf8");
-  
-    return conn;
-  };
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  return conn;
+};
 
   module.exports = { connect };
