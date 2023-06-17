6549cd1a1c74ffaa3066b2d73ce3484d966c0f01
RonnieGuo <guorongfly@gmail.com>
2023-06-17 15:11:16 -0600

    Create the Input Module

---
+++ input.js
@@ -0,0 +1,21 @@
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+// the stdin is set up to handle user input, and pressing Ctrl + C will terminate the game by calling 
+const handleUserInput = function (key) {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+  process.exit();
+  }
+};
+
+
+module.exports = {
+  setupInput,
+};
