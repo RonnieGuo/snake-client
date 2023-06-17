88c2405df8c6860024cee148a1923dc396831593
RonnieGuo <guorongfly@gmail.com>
2023-06-17 14:59:30 -0600

    Handling CTRL + C User Input

--- play.js
+++ play.js
@@ -2,3 +2,22 @@ const { connect } = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+// the stdin is set up to handle user input, and pressing Ctrl + C will terminate the game by calling 
+const handleUserInput = function (key) {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+  process.exit();
+  }
+};
+const stdin = setupInput();
+stdin.on("data", handleUserInput);
