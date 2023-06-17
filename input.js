4fd84b67596ab0b081292dbcea39f4b38856289d
RonnieGuo <guorongfly@gmail.com>
2023-06-17 15:25:06 -0600

    add WASD Movement, and messages

--- input.js
+++ input.js
@@ -1,21 +1,37 @@
-// setup interface to handle user input from stdin
+// Stores the active TCP connection object.
+let connection;
 
-const setupInput = function () {
+const handleUserInput = function (key) {
+  if (key === 'w') {
+    connection.write("Move: up");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  } else if (key === 's') {
+    connection.write("Move: down");
+  } else if (key === 'd') {
+    connection.write("Move: right");
+  } else if (key === '1') {
+    connection.write("Say: Hello!");
+  } else if (key === '2') {
+    connection.write("Say: I'm the best!");
+  } else if (key === '3') {
+    connection.write("Say: I got this one!");
+  }
+};
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  stdin.on("data", (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    handleUserInput(key);
+  });
   return stdin;
 };
-// the stdin is set up to handle user input, and pressing Ctrl + C will terminate the game by calling 
-const handleUserInput = function (key) {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-  process.exit();
-  }
-};
 
-
-module.exports = {
-  setupInput,
-};
+module.exports = { setupInput };
